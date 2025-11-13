e5566f815f765ba060590d0503f11e7f1a44202c
Hazel <dortylp@dunwoody.edu>
2025-11-13 11:54:21 -0600

    initial

---
+++ submitUserInput.js
@@ -0,0 +1,7 @@
+// function getInput() {
+//     let userInputVar = "";
+//     let userInputAll = "placeholder variable";
+//     userInputVar = document.getElementById("userInput");
+//     userInputAll = (userInputAll + " " + userInputVar);
+//     console.log(userInputAll);
+// }
